a78a48e44eb212066e48315d24a1193134e53f6e
jun2vv <cj3696@naver.com>
2024-04-10 19:57:47 +0900

    Customer.js추가 및 데이터 반복출력 연습

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Customer from './components/Customer'
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https//placeimg.com/64/64/1',
+  'name' : '최준',
+  'birthday' : '990920',
+  'gender' : '남자',
+  'job' : '연구원'
+},
+{
+  'id' : 2,
+  'image' : 'https//placeimg.com/64/64/2',
+  'name' : '김채은',
+  'birthday' : '991125',
+  'gender' : '여자',
+  'job' : '멋쟁이'
+},
+{
+  'id' : 3,
+  'image' : 'https//placeimg.com/64/64/3',
+  'name' : '밍밍이',
+  'birthday' : '180000',
+  'gender' : '중성',
+  'job' : '고양이'
+},
+{
+  'id' : 4,
+  'image' : 'https//placeimg.com/64/64/3',
+  'name' : '단추',
+  'birthday' : '180000',
+  'gender' : '중성',
+  'job' : '고양이'
+}  
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="grey-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system! test</h2>
+      <div>
+        { 
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
       </div>
     );
   }
