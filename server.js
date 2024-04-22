acaebbb02211ec467849e7ecc60521b689bcc198
jun2vv <cj3696@naver.com>
2024-04-23 08:59:52 +0900

    db테이블 만들어서 node.js에서 데이터 출력해보기

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,41 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+   host: conf.host,
+   user: conf.user,
+   password: conf.password,
+   port: conf.port,
+   database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers',(req, res) => {
-    res.send([
-        {
-          'id' : 1,
-          'image' : 'https//placeimg.com/64/64/1',
-          'name' : '최준',
-          'birthday' : '990920',
-          'gender' : '남자',
-          'job' : '테스트'
-        },
-        {
-          'id' : 2,
-          'image' : 'https//placeimg.com/64/64/2',
-          'name' : '김채은',
-          'birthday' : '991125',
-          'gender' : '여자',
-          'job' : '멋쟁이'
-        },
-        {
-          'id' : 3,
-          'image' : 'https//placeimg.com/64/64/3',
-          'name' : '밍밍이',
-          'birthday' : '180000',
-          'gender' : '중성',
-          'job' : '고양이'
-        },
-        {
-          'id' : 4,
-          'image' : 'https//placeimg.com/64/64/3',
-          'name' : '단추',
-          'birthday' : '180000',
-          'gender' : '중성',
-          'job' : '고양이'
-        }  
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
